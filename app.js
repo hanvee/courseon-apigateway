e847c3aa513b0ac12a2dd0c93678d4153b33395e
hanvee <farhanram71@gmail.com>
2023-12-19 15:24:04 +0700

    Add integration with media services

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const coursesRouter = require('./routes/courses');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
